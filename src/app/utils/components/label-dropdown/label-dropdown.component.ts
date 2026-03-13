e5576bfb3ef597d52eebfde349cdbafa78e47594
geanCarneiro <geancarneiro1@gmail.com>
2026-03-13 09:37:57 -0300

    mudar layout dos históricos

--- src/app/utils/components/label-dropdown/label-dropdown.component.ts
+++ src/app/utils/components/label-dropdown/label-dropdown.component.ts
@@ -1,19 +1,34 @@
-import { Component, Input } from '@angular/core';
+import { animate, state, style, transition, trigger } from '@angular/animations';
+import { CommonModule, NgTemplateOutlet } from '@angular/common';
+import { Component, ContentChild, Input, TemplateRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-label-dropdown',
   templateUrl: './label-dropdown.component.html',
-  styleUrls: ['./label-dropdown.component.scss']
+  styleUrls: ['./label-dropdown.component.scss'],
+  imports: [CommonModule, NgTemplateOutlet],
+  animations: [
+    trigger('openClose', [
+      transition(':enter', [style({height: 0}), animate('300ms', style({height: '*'}))]),
+      transition(':leave', [animate('300ms', style({height: '0'}))])
+    ])
+  ]
 })
 export class LabelDropdownComponent {
 
-  @Input() selectedLabel: string = '';
-  @Input() items: string[] = [];
+  @Input() items: any[] = [];
+
+  @ContentChild('item', {static: true}) item : TemplateRef<any>
+  @ContentChild('itemList', {static: true}) itemList : TemplateRef<any>
 
   open = false;
 
   toggle() {
     this.open = !this.open;
+  }
+
+  get outros() : any {
+    return this.items.slice(1);
   }
 
 }
